19566ac45d35cbfffd25ba33218e2ce87f461fd2
Anton Latyshev <latyshev.antony@gmail.com>
2026-03-13 01:04:21 +0300

    Add TransactionUtilsTest

---
+++ Server/src/test/java/org/example/util/TransactionUtilsTest.java
@@ -0,0 +1,161 @@
+package org.example.util;
+
+import org.example.dto.Status;
+import org.example.exception.BusinessException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@ExtendWith(MockitoExtension.class)
+public class TransactionUtilsTest {
+    @Mock
+    private SessionBuilder sessionBuilder;
+    @Mock
+    private SessionFactory sessionFactory;
+    @Mock
+    private Session session;
+    @Mock
+    private Transaction transaction;
+
+    @BeforeEach
+    public void setUp() {
+        Mockito.when(sessionBuilder.getSessionFactory()).thenReturn(sessionFactory);
+        Mockito.when(sessionFactory.openSession()).thenReturn(session);
+        Mockito.when(session.beginTransaction()).thenReturn(transaction);
+    }
+
+    @Test
+    public void executeInTransaction_withConsumerAndSuccess_shouldCommit() {
+        Consumer<Session> consumer = Mockito.mock(Consumer.class);
+
+        TransactionUtils.executeInTransaction(sessionBuilder, consumer);
+
+        Mockito.verify(consumer).accept(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction).commit();
+        Mockito.verify(transaction, Mockito.never()).rollback();
+
+    }
+
+    @Test
+    public void executeInTransaction_withConsumerAndBusinessException_shouldNotCommitAndNotRollback() {
+        Consumer<Session> consumer = Mockito.mock(Consumer.class);
+
+        Mockito.doThrow(new BusinessException(Status.SERVER_ERROR, "")).when(consumer).accept(session);
+
+        Assertions.assertThrows(BusinessException.class, () -> {
+            TransactionUtils.executeInTransaction(sessionBuilder, consumer);
+        });
+
+        Mockito.verify(consumer).accept(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction, Mockito.never()).commit();
+        Mockito.verify(transaction, Mockito.never()).rollback();
+    }
+
+    @Test
+    public void executeInTransaction_withConsumerAndRuntimeException_shouldNotCommitAndRollback() {
+        Consumer<Session> consumer = Mockito.mock(Consumer.class);
+
+        Mockito.doThrow(new RuntimeException()).when(consumer).accept(session);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            TransactionUtils.executeInTransaction(sessionBuilder, consumer);
+        });
+
+        Mockito.verify(consumer).accept(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction, Mockito.never()).commit();
+        Mockito.verify(transaction).rollback();
+    }
+
+    @Test
+    public void executeInTransactionReturnFunction() {
+        Function<Session, ?> function = Mockito.mock(Function.class);
+
+        TransactionUtils.executeInTransaction(sessionBuilder, function);
+
+        Mockito.verify(function).apply(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction).commit();
+    }
+
+    @Test
+    public void executeInTransaction_withFunctionAndBusinessException_shouldNotCommitAndNoRollback() {
+        Function<Session, ?> function = Mockito.mock(Function.class);
+
+        Mockito.doThrow(new BusinessException(Status.SERVER_ERROR, "")).when(function).apply(session);
+
+        Assertions.assertThrows(BusinessException.class, () -> {
+            TransactionUtils.executeInTransaction(sessionBuilder, function);
+        });
+
+        Mockito.verify(function).apply(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction, Mockito.never()).commit();
+        Mockito.verify(transaction, Mockito.never()).rollback();
+    }
+
+    @Test
+    public void executeInTransaction_withFunctionAndRuntimeException_shouldNotCommitAndRollback() {
+        Function<Session, ?> function = Mockito.mock(Function.class);
+
+        Mockito.doThrow(new RuntimeException()).when(function).apply(session);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            TransactionUtils.executeInTransaction(sessionBuilder, function);
+        });
+
+        Mockito.verify(function).apply(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction, Mockito.never()).commit();
+        Mockito.verify(transaction).rollback();
+    }
+
+    @Test
+    public void executeInTransaction_withFunctionAndSuccess_shouldReturnValue() {
+        int expectedValue = 10;
+        Function<Session, Integer> function = Mockito.mock(Function.class);
+
+        Mockito.when(function.apply(session)).thenReturn(expectedValue);
+
+        int actualValue = TransactionUtils.executeInTransaction(sessionBuilder, function);
+
+        Assertions.assertEquals(expectedValue, actualValue);
+        Mockito.verify(function).apply(session);
+        Mockito.verify(sessionBuilder).getSessionFactory();
+        Mockito.verify(sessionFactory).openSession();
+        Mockito.verify(session).beginTransaction();
+        Mockito.verify(session).close();
+        Mockito.verify(transaction).commit();
+        Mockito.verify(transaction, Mockito.never()).rollback();
+    }
+}
