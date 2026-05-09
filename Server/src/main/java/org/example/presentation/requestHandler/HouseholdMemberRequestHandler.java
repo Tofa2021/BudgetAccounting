2430168c60a31d2b82677b5a2599497ffb2118cf
Anton Latyshev <latyshev.antony@gmail.com>
2026-05-09 18:19:58 +0300

    Update requests

--- Server/src/main/java/org/example/presentation/requestHandler/HouseholdMemberRequestHandler.java
+++ Server/src/main/java/org/example/presentation/requestHandler/HouseholdMemberRequestHandler.java
@@ -5,8 +5,8 @@ import org.example.application.service.HouseholdMemberService;
 import org.example.dto.model.HouseholdMemberDTO;
 import org.example.dto.request.AuthorizedRequest;
 import org.example.dto.request.RequestAction;
-import org.example.dto.request.household.CreateMemberHouseholdRequest;
-import org.example.dto.request.household.DeleteMemberHouseholdRequest;
+import org.example.dto.request.household.CreateHouseholdMemberRequest;
+import org.example.dto.request.household.DeleteHouseholdMemberRequest;
 import org.example.dto.request.household_member.UpdateHouseholdMemberRoleRequest;
 import org.example.dto.response.Response;
 import org.example.util.DTOMapper;
@@ -19,7 +19,7 @@ public class HouseholdMemberRequestHandler {
     public Response handle(RequestAction action, AuthorizedRequest request, Long userId) {
         return switch (action) {
             case CREATE_HOUSEHOLD_MEMBER ->
-                    Response.created(dtoMapper.toDTO(householdMemberService.create((CreateMemberHouseholdRequest) request), HouseholdMemberDTO.class));
+                    Response.created(dtoMapper.toDTO(householdMemberService.create((CreateHouseholdMemberRequest) request), HouseholdMemberDTO.class));
 
             case UPDATE_HOUSEHOLD_MEMBER_ROLE -> {
                 householdMemberService.updateRole((UpdateHouseholdMemberRoleRequest) request);
@@ -27,7 +27,7 @@ public class HouseholdMemberRequestHandler {
             }
 
             case DELETE_HOUSEHOLD_MEMBER -> {
-                householdMemberService.delete((DeleteMemberHouseholdRequest) request);
+                householdMemberService.delete((DeleteHouseholdMemberRequest) request);
                 yield Response.noContent();
             }
 
