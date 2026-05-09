60bbd2b84b9d4af1683aef3d0096bb3331cc39ad
Anton Latyshev <latyshev.antony@gmail.com>
2026-05-09 20:33:53 +0300

    Delete extra requests

--- Common/src/main/java/module-info.java
+++ Common/src/main/java/module-info.java
@@ -3,14 +3,7 @@ module org.example.common {
     exports org.example.dto.response;
     exports org.example.dto.model;
     exports org.example.enums;
-    exports org.example.dto.request.household;
-    exports org.example.dto.request.operation;
-    exports org.example.dto.request.user;
-    exports org.example.dto.request.account;
-    exports org.example.dto.request.household_member;
     exports org.example;
-    exports org.example.dto.request.account_member;
-    exports org.example.dto.request.category;
     requires static lombok;
     requires jdk.dynalink;
 }
