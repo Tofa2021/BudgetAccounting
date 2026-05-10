05d1eedb883c6924bc9e712c225dad65b0e3a37d
Anton Latyshev <latyshev.antony@gmail.com>
2026-05-10 22:15:07 +0300

    Fix not serializable for RequestEnvelope

--- Common/src/main/java/org/example/request/RequestEnvelope.java
+++ Common/src/main/java/org/example/request/RequestEnvelope.java
@@ -1,6 +1,8 @@
 package org.example.request;
 
-public record RequestEnvelope(String accessToken, Request request) {
+import java.io.Serializable;
+
+public record RequestEnvelope(String accessToken, Request request) implements Serializable {
     public boolean isAuthenticated() {
         return accessToken != null && !accessToken.isBlank();
     }
